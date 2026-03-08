a5c22fff218e4d7d324944963ac68ae447d35149
Shihab177 <shihab7777h@gmail.com>
2026-03-08 09:20:38 +0600

    feat: create responsive banner section

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,11 @@
 import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <Container className="">
-      <div className="bg-shop-light-pink">
-        <h1>hello</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptatem
-        vero, nemo error, praesentium quis minima ad enim natus quae quod illo,
-        nobis cumque odit? Doloremque, vero perspiciatis corrupti laborum fuga
-        harum cum quam. Itaque repudiandae praesentium eos asperiores reiciendis
-        dignissimos animi obcaecati ab! Delectus, laudantium illo dolor quae
-        assumenda voluptatum, esse, culpa modi repellendus commodi tempora neque
-        harum enim consequuntur ea autem aliquam adipisci? Quam mollitia quidem
-        delectus quia rerum, nobis asperiores reiciendis natus! Ipsa eius
-        voluptates minus eaque nobis molestiae doloribus, expedita sunt, quod
-        porro praesentium temporibus deserunt repudiandae autem facilis
-        excepturi commodi debitis sapiente! Aliquam corporis quis sed deserunt
-        perspiciatis minus optio quam dolorum nulla, enim quaerat, non cum!
-        Culpa, ullam eos quibusdam officiis adipisci ipsum aut pariatur quaerat
-        quos soluta mollitia aliquam magnam nulla ad? Veniam quas est cupiditate
-        itaque vero, neque cumque magnam dolorum reiciendis assumenda delectus
-        dolorem repellat illo ratione nesciunt facilis illum, voluptates aut
-        beatae placeat. Commodi qui ex sunt minus quae facere ab quaerat illo,
-        tempora quisquam, minima reiciendis veniam atque deleniti, sequi placeat
-        incidunt? Vitae cumque eligendi quo recusandae facere, expedita fugit
-        pariatur quam perferendis magni! Recusandae, error consequatur iure
-        ipsam alias aspernatur ad consequuntur non perspiciatis molestias minima
-        eligendi tempora quaerat magni molestiae, provident nihil a totam
-        excepturi. A delectus error voluptatum consequatur perferendis, sapiente
-        facilis, quasi officia quo mollitia accusantium maxime eum ex deleniti
-        debitis, consequuntur illo est aliquam autem dignissimos? Beatae ullam
-        magni reiciendis suscipit possimus itaque atque, animi dolore minima non
-        accusantium ducimus incidunt, optio laborum assumenda aperiam odio
-        necessitatibus modi. Animi maxime nostrum reprehenderit explicabo iusto
-        eum! Culpa, iste soluta! Impedit veniam deleniti asperiores nemo optio
-        debitis voluptatem, quasi saepe praesentium fugit consectetur cumque,
-        voluptate hic nam ullam obcaecati veritatis, culpa deserunt recusandae
-        repellat? Saepe repudiandae nobis repellat magni autem beatae nam
-        doloremque asperiores vel perferendis.
-      </div>
+    <HomeBanner/>
     </Container>
   );
 }
